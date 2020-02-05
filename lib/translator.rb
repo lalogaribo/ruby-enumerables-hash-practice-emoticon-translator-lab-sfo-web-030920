df68e7ef3ede3e95ddb369e755090718d0e8fcb2
Eduardo mendoza <garibo.eduardo90@gmail.com>
2020-02-05 00:52:06 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,9 +3,14 @@ require 'yaml'
 
  
 
-def load_library
- thing = YAML.load_file('emoticons.yml')
-pp thing
+def load_library(file)
+ response = {"meaning" => {}, "emoticon" => {}}
+ library = YAML.load_file(file)
+ library.each do |trans, emoticon|
+   response["meaning"][emoticon[1]] = trans
+   response["emoticon"][emoticon[0]] = emoticon[0]
+ end
+ response
 end
 
 def get_japanese_emoticon
